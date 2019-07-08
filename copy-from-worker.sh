5c9cfbcb72c903cd29368dff9b2a5cf8f49dffba
Sergey Anisiforov <sa@payneteasy.com>
2019-07-08 17:23:32 +0300

    Improved framework updating script

--- copy-from-worker.sh
+++ copy-from-worker.sh
@@ -2,13 +2,19 @@
 
 set -eux
 
-ID=103798
+ID=104235
 #wget -O Reader/libPaynetEasyReader.a   http://paynet-qa.clubber.me/reader-ios/libPaynetEasyReader-${ID}.a
 wget -O PaynetEasyReader.framework.zip http://paynet-qa.clubber.me/reader-ios/PaynetEasyReader.framework-${ID}.zip
 
 rm -rf unzip
+rm -rf PaynetEasyReader.framework
 unzip PaynetEasyReader.framework.zip -d unzip
 mv unzip/Carthage/Build/iOS/PaynetEasyReader.framework PaynetEasyReader.framework
 rm -rf unzip
+
+cd PaynetEasyReader.framework
+lipo -remove x86_64 PaynetEasyReader -o PaynetEasyReader
+lipo -remove i386 PaynetEasyReader -o PaynetEasyReader
+cd ..
 
 cat how-to-release.txt
